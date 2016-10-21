159874d26bd455b966c3615e5b0de61d13475c92
song-li <993793072@qq.com>
2016-10-21 18:22:24 -0400

    swap and smallest nsIRunnable not tested

--- xpcom/threads/nsEventQueue.h
+++ xpcom/threads/nsEventQueue.h
@@ -8,6 +8,7 @@
 #define nsEventQueue_h__
 
 #include <stdlib.h>
+#include <vector>
 #include "mozilla/CondVar.h"
 #include "mozilla/Mutex.h"
 #include "nsIRunnable.h"
@@ -41,6 +42,13 @@ public:
   bool GetEvent(bool aMayWait, nsIRunnable** aEvent,
                 MutexAutoLock& aProofOfLock);
 
+  //SECLAB Tue 18 Oct 2016 11:19:12 AM EDT START
+  //This function is used for find a flag runnable and swap this flag runnable with a 
+  //real runnable. This function will work together with GetFlag() function
+  bool SecSwapRunnable(nsIRunnable* runnable, uint64_t expTime, MutexAutoLock& aProofOfLock);
+  //SECLAB Tue 18 Oct 2016 11:20:10 AM EDT END
+
+
   // This method returns true if there is a pending event.
   bool HasPendingEvent(MutexAutoLock& aProofOfLock)
   {
@@ -56,6 +64,97 @@ public:
   size_t Count(MutexAutoLock&);
 
 private:
+  //SECLAB Tue 18 Oct 2016 11:23:11 AM EDT START
+  nsIRunnable** GetFlag(const uint64_t expTime) {
+    Page* head = mHead;
+    int offset = mOffsetHead;
+    while(head != mTail && offset != mOffsetTail) {
+      if(offset == EVENTS_PER_PAGE) {
+        offset = 0;
+        head = head->mNext;
+      }else {
+        if(head->mExpTime[offset] == expTime) {
+          return &(head->mEvents[offset]);
+        }
+        offset ++;
+      }
+    }
+    return NULL;
+  }
+  //SECLAB Tue 18 Oct 2016 11:39:38 AM EDT END
+//SECLAB Thu 13 Oct 2016 02:55:25 PM EDT START
+
+  void GetRunNow() {
+    if(IsEmpty()) return ;
+    Page* head = mHead, *tail = mTail, *nextRun = NULL;
+    uint16_t offset = -1;
+    int first_end = 0;
+    uint64_t minExpTime = 2147483648;// larger than the normal int
+    const bool debug = 0;
+    std::vector<uint64_t > debugQueue;
+    //printf("%lld headheadhead\n", mHead->mEvents[mOffsetHead]->expTime);
+
+    if(head != tail) first_end = EVENTS_PER_PAGE;
+    else first_end = mOffsetTail;
+    for(int i = mOffsetHead;i < first_end;++ i){
+      debugQueue.push_back(head->mExpTime[i]);
+      if(head->mExpTime[i] < minExpTime){
+        nextRun = head;
+        offset = i;
+        minExpTime = head->mExpTime[i];
+      }
+    }
+
+    if(head != tail) head = head->mNext;
+
+    while(head != tail) {
+      for(int i = 0;i < EVENTS_PER_PAGE;++ i){
+        debugQueue.push_back(head->mExpTime[i]);
+        if(head->mExpTime[i] < minExpTime){
+          nextRun = head;
+          offset = i;
+          minExpTime = head->mExpTime[i];
+        }
+      }
+      head = head->mNext;
+    }
+
+    if(mHead != mTail) {
+      for(int i = 0;i < mOffsetTail;++ i) {
+        debugQueue.push_back(tail->mExpTime[i]);
+        if(tail->mExpTime[i] < minExpTime){
+          nextRun = tail;
+          offset = i;
+          minExpTime = head->mExpTime[i];
+        }
+      }
+    }
+
+    if(mHead != head || mOffsetHead != offset) {
+      nsIRunnable* tmp = mHead->mEvents[mOffsetHead];
+      mHead->mEvents[mOffsetHead] = nextRun->mEvents[offset];
+      nextRun->mEvents[offset] = tmp;
+
+      int expTime = mHead->mExpTime[mOffsetHead];
+      mHead->mExpTime[mOffsetHead] = nextRun->mExpTime[offset];
+      nextRun->mExpTime[offset] = expTime;
+
+      if(debug) {
+        for(auto it = debugQueue.begin(); it != debugQueue.end();++ it) {
+          printf("%lu ",*it);
+        }
+        printf("Current %lu %lu \n", mHead->mExpTime[mOffsetHead], head->mExpTime[offset]);
+      }
+    }
+
+    //bool f = mHead->flag[mOffsetHead];
+    //mHead->flag[mOffsetHead] = nextRun->flag[offset];
+    //nextRun->flag[offset] = t;
+  }
+  //SECLAB Thu 13 Oct 2016 02:55:29 PM EDT END
+
+  uint64_t curTime = 1e8;
+
   bool IsEmpty()
   {
     return !mHead || (mHead == mTail && mOffsetHead == mOffsetTail);
@@ -71,11 +170,16 @@ private:
   struct Page
   {
     struct Page* mNext;
+    //SECLAB Mon 17 Oct 2016 07:13:40 PM EDT START
+    //bool flag[EVENTS_PER_PAGE + 1];
+    uint64_t mExpTime[EVENTS_PER_PAGE + 1];
+    //SECLAB Mon 17 Oct 2016 07:13:43 PM EDT END
+
     nsIRunnable* mEvents[EVENTS_PER_PAGE];
   };
 
   static_assert((sizeof(Page) & (sizeof(Page) - 1)) == 0,
-                "sizeof(Page) should be a power of two to avoid heap slop.");
+      "sizeof(Page) should be a power of two to avoid heap slop.");
 
   static Page* NewPage()
   {
