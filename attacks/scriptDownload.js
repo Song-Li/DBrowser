7a7c5b1373139d09947579f37973b69fe7616f4a
song-li <993793072@qq.com>
2016-11-01 16:16:25 -0400

    added attack to zhanhao

--- attacks/scriptDownload.js
+++ attacks/scriptDownload.js
@@ -1,14 +1,14 @@
 function run() {
   var element_s = document.createElement('script');
   document.body.appendChild(element_s);
-  var start = performance.now();
-  element_s.src = "./1e2.js";
-  document.getElementById("start").innerHTML = "start at " + start;
   window.onerror = function() {
     var end = performance.now();
     document.getElementById("end").innerHTML = "end " + end;
     document.getElementById("info").innerHTML = "time to downlode " + (end - start);
   }
+  var start = performance.now();
+  document.getElementById("start").innerHTML = "start at " + start;
+  element_s.src = "./1e4.js";
   element_s.onload = function() {
     console.log("onload");
   }
