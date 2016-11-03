a9e55a8a924bd11ab3e3ec884ddecb60f951223c
sol315 <song-li>
2016-11-03 15:18:02 -0400

    changed to a more accuracy file

--- attacks/gen_payload.py
+++ attacks/gen_payload.py
@@ -1,6 +1,6 @@
 
 for size in range(1, 21):
-    payload = size * 1024 * 512 * 5
+    payload = size * 1024 * 512 * 1
     print size
     f = open(str(size * 1) + 'M.js', 'w')
     for i in range(int(payload)):
