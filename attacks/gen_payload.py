e7d39c4ad91bbd35de57de350d1e579f895c2c5c
sol315 <song-li>
2016-11-02 21:45:40 -0400

    added useless 0M

--- attacks/gen_payload.py
+++ attacks/gen_payload.py
@@ -1,6 +1,7 @@
 
 for size in range(1, 21):
     payload = size * 1e5 / 1.2
+    print size
     f = open(str(size) + 'M.js', 'w')
     for i in range(int(payload)):
         f.write("hello world\n")
