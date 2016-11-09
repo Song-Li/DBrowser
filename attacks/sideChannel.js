d6414ac82750fb5a67230fd941c7eebbd6f81b3b
song-li <993793072@qq.com>
2016-11-09 16:35:31 -0500

    working

--- attacks/sideChannel.js
+++ attacks/sideChannel.js
@@ -3,6 +3,20 @@ var buffer = new ArrayBuffer(size);
 var buffer2 = new ArrayBuffer(size);
 var origindata = new DataView(buffer);
 var freshdata = new DataView(buffer2);
+var start;
+var type = 0;
+
+startPostMessage = function(){
+  var receiveMessage = function(message){
+    if (message.data !== 'Test') return;
+    getEnd();
+  };
+  var postMessage = function() {
+    window.postMessage('Test', window.location.toString())
+  };
+  window.addEventListener("message", receiveMessage, false);
+  postMessage();
+}
 
 function set() {
   var pointer = 0;
@@ -22,21 +36,24 @@ function fresh() {
   }while(pointer < size);
 }
 
+function getEnd() {
+  var end = performance.now();
+  if(type == 1) document.write("Diff: ");
+  else document.write("Same: ");
+  document.write("Start " + start + " end " + end + "<br>");
+  document.write(end - start);
+}
+
 function check() {
   var bit = 1;
   var pointer = 0;
-  var cnt = 1e4;
-  var type = 1;
+  var cnt = 1e5;
   set();
-  var start = performance.now();
+  start = performance.now();
+  startPostMessage();
   while(cnt --) {
     fresh();
     if(type == 1) set();
     else fresh();
   }
-  var end = performance.now();
-  if(type == 1) document.write("Diff: ");
-  else document.write("Same: ");
-  document.write("start " + start + " end " + end + "<br>");
-  document.write(end - start);
 }
