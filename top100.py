0403e252090b4f51bea1c880fb83c13dedbd5607
nkdxczh <zhc416@lehigh.edu>
2017-03-29 16:28:52 -0400

    stable version

--- top100.py
+++
@@ -1,9 +0,0 @@
-from catcher import catch
-
-base = 'http://www.alexa.com/topsites/global;'
-c = catch()
-for i in range(20):
-    url = base + str(i)
-    print(url)
-    c.set_url(url)
-    c.get_href()
